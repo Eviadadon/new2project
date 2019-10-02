44c5cd6f6106ddbbd1a231dce147efea759f18c2
eviadadon <ad9372@bs.amalnet.k12.il>
2019-10-02 10:06:38 +0200

    fhg

--- app/src/main/java/com/example/aratzot/MainActivity.java
+++ app/src/main/java/com/example/aratzot/MainActivity.java
@@ -1,30 +1,51 @@
-package com.example.aratzot;
+package com.example.aratzot ;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Adapter;
 import android.widget.AdapterView;
-import android.widget.Button;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    ListView lv ;
-    Button bt ;
-    String[] Yabeshet= {"Asia","Europe","Africa","America"};
+public class MainActivity extends AppCompatActivity implements OnItemClickListener {
+    ListView lv;
+    String[] yabeshet = {"Europe", "America", "Asia", "Africa"};
+    String choice;
+    int ch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv = (ListView) findViewById(R.id.lv);
-        bt = (Button) findViewById(R.id.bt)
-        lv.setOnItemClickListener(this
-        );
-        
+        lv = (ListView) findViewById(R.id.LV);
+        lv.setOnItemClickListener( this);
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item );
+        lv.setAdapter(adp);
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast toast = Toast.makeText(this,""+yabeshet[position],Toast.LENGTH_SHORT);
+        toast.show();
+        choice =yabeshet [position];
+        ch= position;
+
 
     }
+
+    public void nextact(View view) {
+        Intent gi= new Intent(this,Main2Activity.class);
+        gi.putExtra("n:", ch);
+        startActivity(gi);
+    }
 }
+
